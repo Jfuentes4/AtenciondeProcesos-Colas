945b4bc654edbd2ba1a9b16755d4d3e7e068c81e
JFuentes4 <otro64@hotmail.com>
2019-11-05 15:21:35 -0600

    Atencion de procesos por encolados

---
+++ src/js/PseudoProcessor.js
@@ -0,0 +1,58 @@
+import Process from './process.js';
+
+class PseudoProcessor {
+    constructor (cycles) {
+        this._cycles = cycles;
+        this._stack = [];
+    }
+
+    startProcessing = () => {
+        let emptyTimes = 0;
+        let maxStackLength = 0;
+        let attendedProcess = 0;
+        let currentProcess = undefined;
+        this._stack = [];
+
+        for (let i = 0; i < this._cycles; i++) {
+            this._spawnProcess();
+            if (currentProcess !== undefined) {
+                if (currentProcess.remainingCycles !== 0) {
+                    currentProcess.remainingCycles--;
+
+                } else {
+                    attendedProcess++;
+                    currentProcess = this._stack.pop();
+                }
+            } else {
+                emptyTimes++;
+                currentProcess = this._stack.pop();
+            }
+
+            if (this._stack.length > maxStackLength) {
+                maxStackLength = this._stack.length;
+            }
+
+
+        }
+        let remainingProcess = this._stack.length;
+        let remainingCycles = this._stack.reduce((accumulator, currentValue) => accumulator + currentValue.remainingCycles, 0);
+        let result = {
+            emptyTimes,
+            maxStackLength,
+            remainingCycles,
+            remainingProcess,
+            attendedProcess,
+        }
+
+        return result;
+    };
+
+    _spawnProcess = () => {
+        let probability = Math.ceil(Math.random() * 100);
+        if (probability <= 39) {
+            this._stack.push(new Process());
+        }
+    }
+}
+
+export default PseudoProcessor;
